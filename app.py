eccd8a814ffa8c6672e5f843db38d6454ab30b36
David Lin <d2955388365@gmail.com>
2022-05-21 21:53:08 +0800

    2 week version2

--- app.py
+++ app.py
@@ -291,5 +291,5 @@ def addCart():
 
 
 
-app.run(port=3000,debug=True)
-# app.run(host='0.0.0.0', port=3000,debug=True)
+# app.run(port=3000,debug=True)
+app.run(host='0.0.0.0', port=3000,debug=True)
