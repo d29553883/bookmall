537ac77c26980ccf95cb0cbf58fbfd1d2f33a102
David Lin <d2955388365@gmail.com>
2024-02-27 20:25:48 +0800

    fix connection

--- tests/test_bookId.py
+++ tests/test_bookId.py
@@ -3,10 +3,11 @@ from model.bookId import BookIdModel
 
 
 bookid_model = BookIdModel()
-@pytest.mark.parametrize("book_id", [309, 513])
+@pytest.mark.parametrize("book_id", [309, 348, 513])
 def test_searchid(book_id):
   book_info, _= bookid_model.searchid(book_id)
   assert isinstance(book_info, dict)
   assert "bookid" in book_info
   assert "author" in book_info
+  assert "category" in book_info
   assert isinstance(book_info["data"], list)
